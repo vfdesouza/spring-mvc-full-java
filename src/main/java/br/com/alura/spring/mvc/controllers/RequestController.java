b020f159c8fb5f80fc4d66e679a65d7190fdef82
vfdesouza <vfdesouza@outlook.com>
2022-06-19 14:41:39 -0300

    implementation of the filter buttons function by order status

--- src/main/java/br/com/alura/spring/mvc/controllers/RequestController.java
+++ src/main/java/br/com/alura/spring/mvc/controllers/RequestController.java
@@ -1,14 +1,17 @@
 package br.com.alura.spring.mvc.controllers;
 
 import br.com.alura.spring.mvc.models.Request;
+import br.com.alura.spring.mvc.models.StatusRequest;
 import br.com.alura.spring.mvc.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class RequestController {
@@ -32,6 +35,18 @@ public class RequestController {
         ModelAndView mv = new ModelAndView("home");
         mv.addObject("requests", rr.findByProductNameIgnoreCaseContaining(productName));
         return mv;
+    }
+
+    @GetMapping("home/{statusRequest}")
+    public String status(@PathVariable("statusRequest") String statusRequest, Model model) {
+        List<Request> requests = rr.findByStatusRequest(StatusRequest.valueOf(statusRequest.toUpperCase()));
+        model.addAttribute("requests", requests);
+        model.addAttribute("statusRequest", statusRequest);
+        return "home";
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String onError() {
+        return "redirect:/home";
     }
 
     @RequestMapping(value = "/formCad", method = RequestMethod.POST)
--- src/main/java/br/com/alura/spring/mvc/repository/RequestRepository.java
+++ src/main/java/br/com/alura/spring/mvc/repository/RequestRepository.java
@@ -1,6 +1,7 @@
 package br.com.alura.spring.mvc.repository;
 
 import br.com.alura.spring.mvc.models.Request;
+import br.com.alura.spring.mvc.models.StatusRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface RequestRepository extends JpaRepository<Request, Long> {
     List<Request> findByProductNameIgnoreCaseContaining(String productName);
+    List<Request> findByStatusRequest(StatusRequest statusRequest);
 }
