a8dadb0e31c33d2e60f39c45ece4223fa07790e6
vfdesouza <vfdesouza@outlook.com>
2022-06-21 10:46:43 -0300

    correct delegation for the methods contained in the controls and adjustment in the html of the templates

--- src/main/java/br/com/alura/spring/mvc/controllers/HomeController.java
+++ src/main/java/br/com/alura/spring/mvc/controllers/HomeController.java
@@ -1,11 +1,43 @@
 package br.com.alura.spring.mvc.controllers;
+import br.com.alura.spring.mvc.models.Request;
+import br.com.alura.spring.mvc.models.StatusRequest;
+import br.com.alura.spring.mvc.repository.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
-    @RequestMapping("/")
-    public String home() {
+
+    @Autowired
+    RequestRepository rr;
+    @GetMapping
+    public ModelAndView findAllOrfindByProductName(@RequestParam(value = "productName", required = false) String productName) {
+      if (productName == null) {
+          ModelAndView mv = new ModelAndView("home");
+          Iterable<Request> requests = rr.findAll();
+          mv.addObject("requests", requests);
+          return mv;
+      }
+      ModelAndView mv = new ModelAndView("home");
+      mv.addObject("requests", rr.findByProductNameIgnoreCaseContaining(productName));
+      return mv;
+  }
+
+    @GetMapping("/{statusRequest}")
+    public String status(@PathVariable("statusRequest") String statusRequest, Model model) {
+        List<Request> requests = rr.findByStatusRequest(StatusRequest.valueOf(statusRequest.toUpperCase()));
+        model.addAttribute("requests", requests);
+        model.addAttribute("statusRequest", statusRequest);
         return "home";
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String onError() {
+        return "redirect:/home";
     }
 }
--- src/main/java/br/com/alura/spring/mvc/controllers/RequestController.java
+++ src/main/java/br/com/alura/spring/mvc/controllers/RequestController.java
@@ -5,51 +5,23 @@ import br.com.alura.spring.mvc.models.StatusRequest;
 import br.com.alura.spring.mvc.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
+@RequestMapping("formCad")
 public class RequestController {
-
     @Autowired
     private RequestRepository rr;
 
-    @RequestMapping(value ="/formCad", method = RequestMethod.GET)
+    @GetMapping
     public String form(Request request) {
         return "formCad";
     }
 
-    @RequestMapping("/home")
-    public ModelAndView findAllOrfindByProductName(@RequestParam(value = "productName", required = false) String productName) {
-        if(productName == null) {
-            ModelAndView mv = new ModelAndView("home");
-            Iterable<Request> requests = rr.findAll();
-            mv.addObject("requests", requests);
-            return mv;
-        }
-        ModelAndView mv = new ModelAndView("home");
-        mv.addObject("requests", rr.findByProductNameIgnoreCaseContaining(productName));
-        return mv;
-    }
-
-    @GetMapping("home/{statusRequest}")
-    public String status(@PathVariable("statusRequest") String statusRequest, Model model) {
-        List<Request> requests = rr.findByStatusRequest(StatusRequest.valueOf(statusRequest.toUpperCase()));
-        model.addAttribute("requests", requests);
-        model.addAttribute("statusRequest", statusRequest);
-        return "home";
-    }
-    @ExceptionHandler(IllegalArgumentException.class)
-    public String onError() {
-        return "redirect:/home";
-    }
-
-    @RequestMapping(value = "/formCad", method = RequestMethod.POST)
+    @PostMapping
     public String insert(@Valid Request request, BindingResult result){
         if(result.hasErrors()) {
             return "formCad";
